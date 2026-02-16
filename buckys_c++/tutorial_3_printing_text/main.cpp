1f5ddef8e1e0a9b37ab26ab66ad6de9fcdf09c53
Justin Crippen <jcrippen@Justins-MacBook-Pro.local>
2026-02-16 00:25:19 -0600

    added some homework and some notes

---
+++ buckys_c++/tutorial_3_printing_text/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Boy I love Tacos. \n"; //always tell c++ to new line
+    cout << "and hot sauce \n"; //you can also use endl; this also flushes the buffer, still not always certiain on when to do endl and not.
+    return 0;
+}
